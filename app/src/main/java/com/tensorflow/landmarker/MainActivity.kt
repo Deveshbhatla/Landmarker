ec42a5b50135d2526c3af26b6501b13862813fe6
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2023-07-04 22:08:35 +0530

    added skeleton files

--- app/src/main/java/com/tensorflow/landmarker/MainActivity.kt
+++ app/src/main/java/com/tensorflow/landmarker/MainActivity.kt
@@ -2,10 +2,37 @@ package com.tensorflow.landmarker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(LandmarkDetection())
+        bottomNav = findViewById(R.id.bottomNav)
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.landmarkDetection -> {
+                    loadFragment(LandmarkDetection())
+                    true
+                }
+                R.id.aboutInfo -> {
+                    loadFragment(About())
+                    true
+                }
+
+                else -> {true}
+            }
+
+        }
+
+    }
+    private  fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
