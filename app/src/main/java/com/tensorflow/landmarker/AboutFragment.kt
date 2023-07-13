20ad1ebab3fb0ff824231e10c77ed4e6b8813be1
Deveshbhatla <43680827+Deveshbhatla@users.noreply.github.com>
2023-07-14 00:38:38 +0530

    added about section

--- app/src/main/java/com/tensorflow/landmarker/AboutFragment.kt
+++ app/src/main/java/com/tensorflow/landmarker/AboutFragment.kt
@@ -1,11 +1,18 @@
 package com.tensorflow.landmarker
 
-import android.app.ActionBar
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +42,81 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val cardviewButtonEmail=view.findViewById<CardView>(R.id.cardview_button_contact_me)
+        cardviewButtonEmail.setOnClickListener()
+        {
+            val intent = Intent(Intent.ACTION_VIEW)
+            val subject="Contacting via Landmarker Applicaion"
+            val data: Uri = Uri.parse("mailto:contact@deveshbhatla.com?subject=" + Uri.encode(subject))
+            intent.data = data
+            startActivity(intent)
+        }
+
+
+        val cardviewButtonWebsite=view.findViewById<CardView>(R.id.cardview_button_website)
+        cardviewButtonWebsite.setOnClickListener()
+        {
+            val uri = Uri.parse("http://www.deveshbhatla.com")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        val rootView: View = inflater.inflate(R.layout.popup_layout, null)
+        val dialogBuilder = AlertDialog.Builder(context)
+        dialogBuilder.setView(rootView)
+        val dialog = dialogBuilder.create()
+
+        val popupTextBox : TextView=rootView.findViewById(R.id.textView_popup_dialog)
+        val closePopup :Button=rootView.findViewById(R.id.button_dismiss_PopUp)
+
+
+        val cardviewButtonPrivacyPolicy=view.findViewById<CardView>(R.id.cardview_button_privacy_policy)
+        cardviewButtonPrivacyPolicy.setOnClickListener()
+        {
+            dialog.setTitle("Privacy Policy")
+            popupTextBox.text = resources.getString( R.string.privacyPolicy )
+
+            dialog.show()
+
+            closePopup.setOnClickListener()
+            {
+                dialog.dismiss()
+            }
+        }
+
+        val cardviewButtonTermsAndConditions=view.findViewById<CardView>(R.id.cardview_button_terms_and_conditions)
+        cardviewButtonTermsAndConditions.setOnClickListener()
+        {
+            dialog.setTitle("Terms and Conditions")
+            popupTextBox.text = resources.getString( R.string.termsAndConditions)
+
+            dialog.show()
+
+            closePopup.setOnClickListener()
+            {
+                dialog.dismiss()
+            }
+        }
+
+        val cardviewButtonLicense=view.findViewById<CardView>(R.id.cardview_button_license)
+        cardviewButtonLicense.setOnClickListener()
+        {
+            dialog.setTitle("Apache License 2.0\n")
+            popupTextBox.text = resources.getString( R.string.license)
+
+            dialog.show()
+
+            closePopup.setOnClickListener()
+            {
+                dialog.dismiss()
+            }
+
+        }
+
+
+        return view
     }
 
     companion object {
